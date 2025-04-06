67502161aac5c16627c2426aa78b15b82571f4fb
shunkun89 <shunkun4589@gmail.com>
2025-04-06 18:24:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,8 @@ def index():
         else:
             result = {}
     return render_template("index.html", result=result)
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
